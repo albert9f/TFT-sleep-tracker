7ebcd414afceac3be7b137168ba8d15e92c5cf03
albert9f <119642125+albert9f@users.noreply.github.com>
2025-10-10 13:30:58 -0700

    Update UpdateCheckIntervalDays constant from 7 to 2 in UpdateService.cs

--- TFTSleepTracker.Core/Update/UpdateService.cs
+++ TFTSleepTracker.Core/Update/UpdateService.cs
@@ -4,9 +4,9 @@ namespace TFTSleepTracker.Core.Update
 {
     public class UpdateService
     {
-        // Change the UpdateCheckIntervalDays constant from 7 to 2 days
+        // Change the UpdateCheckIntervalDays constant to 2
         private const int UpdateCheckIntervalDays = 2;
-
+        
         // Other existing code...
     }
 }
